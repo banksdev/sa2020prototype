670abc231bc375c05f377404a82e2eebbed84ae6
christoffernissen <christoffer.nissen@gmail.com>
2020-03-20 11:21:33 +0100

    forgot to add files...

---
+++ RabbitMQ/API/Controllers/PlagController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlagController : ControllerBase
+    {
+
+        // POST: api/Plag
+        [HttpPost]
+        public IActionResult Post([FromBody] string filetext)
+        {
+            var textbytes = Encoding.ASCII.GetBytes(filetext);
+            var request = WebRequest.Create($"https://plag_service/api/files");
+            request.Credentials = CredentialCache.DefaultCredentials;
+            request.Method = "POST";
+            request.ContentLength = textbytes.Length;
+
+            Stream requestStream = request.GetRequestStream();
+            requestStream.Write(textbytes, 0, textbytes.Length);
+            requestStream.Close();
+
+            var response = request.GetResponse();
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+
+            string responseFromServer = null;
+            using (var responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream);
+                responseFromServer = reader.ReadToEnd();
+                Console.WriteLine(responseFromServer);
+            }
+
+            response.Close();
+
+            return Ok(responseFromServer);
+        }
+
+    }
+}
---
+++ RabbitMQ/FileMS/Service.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace FileMS
+{
+    public class Service
+    {
+        public string GetFile(Guid id)
+        {
+            Thread.Sleep(200);
+            return "Hello JSON";
+        }
+
+        public Guid CreateFile(string json)
+        {
+            Thread.Sleep(1000);
+            return Guid.NewGuid();
+        }
+    }
+}
